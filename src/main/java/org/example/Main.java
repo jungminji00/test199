991a9528d9f74a7f6c7a0412878881ceed164f11
깃jungminji00 <mg4098@naver.com>
2023-02-22 10:20:40 +0900

    Changes

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+// 문제 : `안녕하세요.`를 10번 출력해주세요.
+// 조건 : System.out.println은 딱 한번만 사용 가능
+// 조건 : \를 활용해서 줄바꿈 해주세요.
+// 힌트 : \n 은 줄바꿈을 의미합니다.
+
+class Main {
+    public static void main(String[] args) {
+        for(int i=0;i<10;i++){
+            System.out.println("안녕하세요.");
+        }
+    }
+}
